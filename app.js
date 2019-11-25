224781f8c1c39095d3e9d495639cdd6d21137de2
Jocelyn Chang <jocelync1094@gmail.com>
2019-11-24 20:41:14 -0800

    Rendered manger input info onto a html

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // all the questions being asked
-const questions = [
+const initialQuestions = [
   {
     type: "input",
     name: "name",
@@ -28,31 +28,110 @@ const questions = [
     type: "input",
     message: "What is the manger's office number?",
     name: "officeNumber", 
+  },
+  {
+    type: "checkbox",
+    message: "What type of team member would you like to add?",
+    name: "member", 
+    choices: ["engineer","intern","done"]
+  }
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?"
+  },
+  {
+    type: "input",
+    message: "What is the employee id?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the employee's email?",
+    name: "email", 
+  },
+  {
+    type: "input",
+    message: "What is the employee's github username?",
+    name: "github", 
+  }
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?"
+  },
+  {
+    type: "input",
+    message: "What is the employee id?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the employee's email?",
+    name: "email", 
+  },
+  {
+    type: "input",
+    message: "What school is the employee going to?",
+    name: "school", 
   }
 ];
 
 inquirer
-  .prompt(questions)
+  .prompt(initialQuestions)
   .then(function(user) {
     
+    const templateMainFile = fs.readFileSync(`./templates/main.html`,  { encoding: 'utf8' });
+
     //first the manager card
     const manager = new Manager (user.name , user.id , user.email , user.officeNumber);
     
-    const templateManagerFile = fs.readFileSync('./templates/manager.html',  { encoding: 'utf8' });
-    let temporaryFile = templateManagerFile.replace('{{ name }}', manager.name);
-    temporaryFile = temporaryFile.replace('{{ role }}', manager.getRole());
-    temporaryFile = temporaryFile.replace('{{ id }}',manager.id);
-    temporaryFile = temporaryFile.replace('{{ email }}',manager.email);
-    temporaryFile = temporaryFile.replace('{{ officeNumber }}',manager.officeNumber);
-    console.log(temporaryFile);
-    
+    const managerCard = renderHTML(manager);
+
+    console.log(managerCard);
+
+    let temporaryMainFile = templateMainFile.replace('{{ team }}', managerCard);
+
+    //adding team members
+    switch(user.member[0]){
+      case "engineer":
+      inquirer.prompt(engineerQuestions)
+      .then(function(data){
+        let engineer1 = new Engineer(data.name , data.id, data.email,data.github);
+        let engineer1card = renderHTML(engineer1);
+        console.log(engineer1card);
+      }).catch(err=>console.log(err));
+    }
+    fs.writeFileSync("index.html",temporaryMainFile);
   })
   .catch(err=>console.log(err));
 
-// function readAndRenderHTML (position) {​
-//   const templateFile = fs.readFileSync(`./templates/${position}.html`,  { encoding: 'utf8' });
-// ​
-//   let temporaryFile = templateFile.replace('{{ name }}', 'Bob');
-// ​
-//   temporaryFile = temporaryFile.replace('{{ email }}', 'Bob@subgenius.org')
-//   }
+
+function renderHTML (position){
+    const templateFile = fs.readFileSync(`./templates/${position.getRole().toLowerCase()}.html`,  { encoding: 'utf8' });
+    let temporaryFile = templateFile.replace('{{ name }}', position.name);
+    temporaryFile = temporaryFile.replace('{{ role }}', position.getRole());
+    temporaryFile = temporaryFile.replace('{{ id }}', position.id);
+    temporaryFile = temporaryFile.replace('{{ email }}', position.email);
+    temporaryFile = temporaryFile.replace('{{ email }}', position.email);
+
+
+    if(position.getRole().toLowerCase()==="engineer"){
+        temporaryFile = temporaryFile.replace('{{ github }}', position.github);
+      }else if(position.getRole().toLowerCase()==="intern"){
+        temporaryFile = temporaryFile.replace('{{ school }}', position.school);
+      }else if(position.getRole().toLowerCase()==="manager"){
+        temporaryFile = temporaryFile.replace('{{ officeNumber }}', position.officeNumber);
+    }
+
+    return temporaryFile;
+}
+
+
+  
